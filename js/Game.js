9363c4471ef1261376b584a0421c59bde64717e7
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-04 18:06:10 +0100

    Create initial structure for the game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,42 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+/**
+ * @file Manages the game's state, logic and interactions.
+ * @author Michal Veselka
+ * {@link https://github.com/kalrog-dev}
+ */
+
+/** Class representing the game. */
+class Game {
+  /**
+   * Create an instance of game.
+   * @param {Phrase[]} phrases An array of phrase objects.
+   */
+  constructor(phrases) {
+    this.phrases = phrases;
+    this.activePhrase = null;
+    this.missed = 0;
+  }
+
+  startGame() {
+
+  }
+
+  getRandomPhrase() {
+
+  }
+
+  handleInteraction() {
+
+  }
+
+  removeLife() {
+
+  }
+
+  checkForWin() {
+
+  }
+
+  gameOver() {
+    
+  }
+} 
