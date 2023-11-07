c4d0e38461941a45828d39293a3c1482f59bfc5b
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-07 02:07:57 +0100

    Add listener for the start button and onscreen keyboard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+/**
+ * @file Adds listeners to enable user interaction and creates an instance of the game.
+ * @author Michal Veselka
+ * {@link https://github.com/kalrog-dev}
+ */
+
 // Phrases to display and guess.
 const data = [
   "Phrase one",
@@ -11,5 +17,8 @@ const data = [
 const phrases = data.map(phrase => new Phrase(phrase));
 const game = new Game(phrases);
 
-// Hide the start overlay and display a random phrase.
-game.startGame();
+// Event listener for "Start Game" button clicks.
+document.getElementById("btn__reset").addEventListener("click", game.startGame);
+
+// Event listener for onscreen keyboard button clicks.
+document.getElementById("qwerty").addEventListener("click", game.handleInteraction);
